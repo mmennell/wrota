0cc06332a44a6ceda160cb27ddd46219f1d0b668
Raghu Betina <raghu@firstdraft.com>
2016-11-26 19:20:12 +0000

    Direct association between restaurants and staffs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :staffs,
+             :foreign_key => "home_restaurant_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations

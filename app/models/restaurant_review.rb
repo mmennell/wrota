b24952ebed6a0d620396cb8ae34199d5d340630e
Raghu Betina <raghu@firstdraft.com>
2016-11-26 19:20:12 +0000

    Direct association between shifts and restaurant_reviews

--- app/models/restaurant_review.rb
+++ app/models/restaurant_review.rb
@@ -1,6 +1,8 @@
 class RestaurantReview < ApplicationRecord
   # Direct associations
 
+  belongs_to :shift
+
   belongs_to :restaurant
 
   # Indirect associations
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,9 @@
 class Shift < ApplicationRecord
   # Direct associations
 
+  has_one    :restaurant_review,
+             :dependent => :destroy
+
   has_one    :staff_review,
              :dependent => :destroy
 

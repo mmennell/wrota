d2300364e2f3846645d775f6295cf51c3d90e78a
Raghu Betina <raghu@firstdraft.com>
2016-11-26 19:20:32 +0000

    Whitelist columns in Staff for ActiveAdmin

--- app/admin/staff.rb
+++ app/admin/staff.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Staff do
 
+ permit_params :firstname, :lastname, :contact_telephone, :home_restaurant_id, :photo, :role_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

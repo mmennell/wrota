65b8b7afc6743d6af05e762842f823d13aaea166
Raghu Betina <raghu@firstdraft.com>
2016-11-26 19:20:12 +0000

    Direct association between shifts and staff_reviews

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,9 @@
 class Shift < ApplicationRecord
   # Direct associations
 
+  has_one    :staff_review,
+             :dependent => :destroy
+
   belongs_to :staff
 
   belongs_to :role
--- app/models/staff_review.rb
+++ app/models/staff_review.rb
@@ -1,6 +1,8 @@
 class StaffReview < ApplicationRecord
   # Direct associations
 
+  belongs_to :shift
+
   belongs_to :staff
 
   # Indirect associations
